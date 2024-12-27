6e947f93d20dc548f3384d166db85821e15540b5
Guillaume Becker <guillaume.becker2013@gmail.com>
2024-12-27 11:23:26 +0100

    adding a frog

---
+++ app.js
@@ -0,0 +1,12 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const StartPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+console.log(squares)
+let currentIndex = 2
+
+function moveFrog() {
+    squares[currentIndex].classList.add('frog')
+}
+
+document.addEventListener('keyup', moveFrog)
